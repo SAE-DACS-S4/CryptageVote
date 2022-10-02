5eee559988ce946e32f4fee25a04cacefca007b8
Quentin BERAL <quentin.beral@etu.umontpellier.fr>
2022-10-02 23:28:08 +0200

    test tirer R

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,2 +1,12 @@
+import java.math.BigInteger;
+import java.util.HashMap;
+
 public class Encrypt {
+    public Encrypt(HashMap<String, BigInteger> pk, BigInteger message) {
+    }
+
+
+    public BigInteger tirerR() {
+        return null;
+    }
 }
--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,4 +9,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EncryptTest {
 
+    KeyGen keyGen = new KeyGen(50);
+
+    HashMap<String, BigInteger> pk = keyGen.getPublicKey();
+
+    BigInteger message = new BigInteger("1");
+
+    Encrypt encrypt = new Encrypt(pk, message);
+
+    @Test
+    public void tirerR() {
+        BigInteger r = encrypt.tirerR();
+
+        //on teste si r est bien compris entre 0 et p'-1
+        BigInteger p = pk.get("p");
+        BigInteger pPrime = (p.subtract(BigInteger.ONE)).divide(BigInteger.valueOf(2));
+        assertTrue(r.compareTo(BigInteger.ZERO) >= 0 && r.compareTo(pPrime.subtract(BigInteger.ONE)) <= 0, "r n'est pas compris entre 0 et p'-1");
+    }
 }
