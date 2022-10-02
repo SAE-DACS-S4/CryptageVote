db865f230031840ebf8eead5abb4b643dab8ba3c
Quentin BERAL <quentin.beral@etu.umontpellier.fr>
2022-10-02 22:59:49 +0200

    bug fix

--- src/main/java/KeyGen.java
+++ src/main/java/KeyGen.java
@@ -5,19 +5,20 @@ import java.util.Random;
 public class KeyGen {
 
     private final int taille;
-    private final BigInteger p;
-    private final BigInteger g;
-    private final BigInteger x;
-    private final BigInteger h;
+    private  BigInteger p;
+    private  BigInteger g;
+    private  BigInteger x;
+    private  BigInteger h;
 
 
     public KeyGen(int taille) {
         this.taille = taille;
 
-        this.p = tirerPremierP();
-        this.g = tirerElementG(p);
-        this.x = tirerEntierX(p);
-        this.h = calculerH(g, x, p);
+        BigInteger p = tirerPremierP();
+        BigInteger g = tirerElementG(p);
+        BigInteger x = tirerEntierX(p);
+        BigInteger h = calculerH(g, x, p);
+
     }
 
     public BigInteger tirerPremierP() {
@@ -28,24 +29,30 @@ public class KeyGen {
             pPrime = (p.subtract(BigInteger.ONE)).divide(BigInteger.valueOf(2));
         }
 
+        this.p = p;
         return p;
     }
 
     public BigInteger tirerElementG(BigInteger p) {
+        BigInteger elementG = new BigInteger(p.bitLength(), new Random());
         boolean gBonneForme = false;
 
         while (!gBonneForme) {
-            BigInteger g = new BigInteger(p.bitLength(), new Random());
-            //test si g est dans l'intervalle [0, p-1]
-            if (g.compareTo(BigInteger.ZERO) > 0 && g.compareTo(p.subtract(BigInteger.ONE)) < 0) {
+            elementG = new BigInteger(p.bitLength(), new Random());
+
+            //test si elementG est dans l'intervalle [0, p-1]
+            if (elementG.compareTo(BigInteger.ZERO) > 0 && elementG.compareTo(p.subtract(BigInteger.ONE)) < 0) {
 
                 BigInteger pPrime = (p.subtract(BigInteger.ONE)).divide(BigInteger.valueOf(2));
-                //test si g^p' = 1 mod p
-                if (g.modPow(pPrime, p).equals(BigInteger.ONE)) {
+                //test si elementG^p' = 1 mod p
+                if (elementG.modPow(pPrime, p).equals(BigInteger.ONE)) {
+
                     gBonneForme = true;
                 }
             }
         }
+
+        this.g = elementG;
         return g;
     }
 
@@ -57,20 +64,23 @@ public class KeyGen {
         while (x.compareTo(BigInteger.ZERO) < 0 || x.compareTo(pPrime.subtract(BigInteger.ONE)) > 0) {
             x = new BigInteger(pPrime.bitLength(), new Random());
         }
+        this.x = x;
         return x;
     }
 
     public BigInteger calculerH(BigInteger g, BigInteger x, BigInteger p) {
-        return g.modPow(x, p);
+        BigInteger h =  g.modPow(x, p);
+        this.h = h;
+        return h;
     }
 
-    public BigInteger getPublicKey() {
+    public HashMap<String, BigInteger> getPublicKey() {
         HashMap<String, BigInteger> publicKey = new HashMap<>();
         publicKey.put("p", p);
         publicKey.put("g", g);
         publicKey.put("h", h);
 
-        return h;
+        return publicKey;
     }
 
     public BigInteger getPrivateKey() {
