bdd7861dd29c517f047faaa521b6390a84fe7659
Quentin BERAL <quentin.beral@etu.umontpellier.fr>
2022-10-02 23:48:06 +0200

    implémentation tirerR

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,13 +1,30 @@
 import java.math.BigInteger;
 import java.util.HashMap;
+import java.util.Random;
 
 public class Encrypt {
+    private HashMap<String, BigInteger> pk;
+    private BigInteger message;
+
     public Encrypt(HashMap<String, BigInteger> pk, BigInteger message) {
+        this.pk = pk;
+        this.message = message;
     }
 
 
     public BigInteger tirerR() {
-        return null;
+        BigInteger pPrime = (this.pk.get("p").subtract(BigInteger.ONE)).divide(BigInteger.valueOf(2));
+
+        Random rand = new Random();
+        BigInteger r = new BigInteger(pPrime.bitLength(), rand);
+
+        //teste si r est dans [0;p'-1]
+        while (r.compareTo(BigInteger.ZERO) < 0 || r.compareTo(pPrime.subtract(BigInteger.ONE)) > 0) {
+            r = new BigInteger(pPrime.bitLength(), rand);
+        }
+
+        System.out.println(r);
+        return r;
     }
 
     public BigInteger encryptU(BigInteger r) {
