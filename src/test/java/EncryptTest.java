2b25c8d46085c3688c7b7c36bcd3a0442042fbff
Quentin BERAL <quentin.beral@etu.umontpellier.fr>
2022-10-02 23:22:45 +0200

    création classe EncryptTest

---
+++ src/test/java/EncryptTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EncryptTest {
+
+}
