28901e29ed0c8f163b8597e28fe216bd758907f8
Quentin BERAL <quentin.beral@etu.umontpellier.fr>
2022-10-02 22:14:46 +0200

    implémentation tirerPremierP

--- src/main/java/KeyGen.java
+++ src/main/java/KeyGen.java
@@ -1,14 +1,29 @@
 import java.math.BigInteger;
+import java.util.Random;
 
 public class KeyGen {
 
+    private int taille;
+    private BigInteger p;
+    private BigInteger g;
+    private BigInteger x;
+    private BigInteger h;
+
 
     public KeyGen(int taille) {
+        this.taille = taille;
     }
 
     public BigInteger tirerPremierP() {
+        BigInteger p = BigInteger.probablePrime(taille, new Random());
+        BigInteger pPrime = (p.subtract(BigInteger.ONE)).divide(BigInteger.valueOf(2));
+        while (!pPrime.isProbablePrime(100)) {
+            p = BigInteger.probablePrime(taille, new Random());
+            pPrime = (p.subtract(BigInteger.ONE)).divide(BigInteger.valueOf(2));
+        }
 
-        return null;
+        this.p = p;
+        return p;
     }
 
     public BigInteger tirerElementG(BigInteger p) {
