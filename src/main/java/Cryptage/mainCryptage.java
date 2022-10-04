318b58eaf4b5d577521c259a92dc5c16b9f973ac
Quentin BERAL <quentin.beral@etu.umontpellier.fr>
2022-10-04 22:59:44 +0200

    update après avoir passé getU et getV en privé

--- src/main/java/Cryptage/mainCryptage.java
+++ src/main/java/Cryptage/mainCryptage.java
@@ -11,14 +11,14 @@ public class mainCryptage {
 
         System.out.println("message 1 = 10");
         MessageCrypte messageCrypte1 = new MessageCrypte(10, publicKey);
-        System.out.println("message crypte 1 : u = " + messageCrypte1.getU() + " v = " + messageCrypte1.getV());
+        //System.out.println("message crypte 1 : u = " + messageCrypte1.getU() + " v = " + messageCrypte1.getV());
 
         System.out.println("message 2 = 20");
         MessageCrypte messageCrypte2 = new MessageCrypte(20, publicKey);
-        System.out.println("message crypte 2 : u = " + messageCrypte2.getU() + " v = " + messageCrypte2.getV());
+        //System.out.println("message crypte 2 : u = " + messageCrypte2.getU() + " v = " + messageCrypte2.getV());
 
         MessageCrypte MessageAgrege = messageCrypte1.agregate(messageCrypte2, publicKey);
-        System.out.println("message agregé : u = " + MessageAgrege.getU() + " v = " + MessageAgrege.getV());
+        //System.out.println("message agregé : u = " + MessageAgrege.getU() + " v = " + MessageAgrege.getV());
 
         System.out.println("message decrypté : " + MessageAgrege.decrypt(privateKey, publicKey));
 
